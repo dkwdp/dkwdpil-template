7294574229f41526c46c0db0deaf27b1db6de34c
Bluemi <bruno.schilling@protonmail.ch>
2026-04-12 11:07:35 +0200

    use label instead of Context.text

--- src/next-scene.ts
+++ src/next-scene.ts
@@ -1,23 +1,18 @@
-import {Context, Scene, Sprite} from 'dkwdpil';
+import {Context, Label, Scene, Sprite} from 'dkwdpil';
 
 const IMAGE_MODES: ("center" | "radius" | "corner")[] = ["center", "radius", "corner"];
 
 export class NextScene extends Scene {
     face: Sprite = new Sprite("face.png", 0, 0, {size: 12.0, imageMode: "center"});
+    touchingLabel: Label = new Label("touching", 0, -16, {fontsize: 1.5, horizAlign: "center", vertAlign: "center"});
     imageModeIndex: number = 0;
-
-    init(c: Context) {
-        c.textAlign(c.CENTER, c.CENTER);
-    }
 
     update(context: Context) {
         context.background(235);
 
         this.face.rotation += 0.01;
 
-        if (this.face.touches(context.mousePos.x, context.mousePos.y, context)) {
-            context.text("touching", 0, -17);
-        }
+        this.touchingLabel.visible = this.face.hovered;
 
         if (this.face.clicked) {
             this.imageModeIndex = (this.imageModeIndex + 1) % IMAGE_MODES.length;
