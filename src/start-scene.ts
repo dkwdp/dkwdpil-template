05fd39ebea006a4cbdc3756f7c2c727f19828c2e
Bluemi <bruno.schilling@protonmail.ch>
2026-04-16 14:41:06 +0200

    add hoverEffect

--- src/start-scene.ts
+++ src/start-scene.ts
@@ -1,10 +1,17 @@
-import {AutoDrawScene, Context, Sprite, Label} from 'dkwdpil';
+import {AutoDrawScene, Context, Sprite, Label, HoverEffect} from 'dkwdpil';
 
 export class StartScene extends AutoDrawScene {
     startSprite: Sprite = new Sprite("playSprite", "play.png", 0, 0, {size: 12.0});
     editSprite: Sprite = new Sprite("editSprite", "edit.png", -30, 16, {size: 3.0});
     eventLabel: Label = new Label("eventsLabel", "Events", -30, 13, {fontsize: 1.0});
     testLabel1: Label = new Label("testLabel", "Example Title", 0, 15, {fontsize: 3.0, horizAlign: "center", vertAlign: "center"});
+
+    hoverEffect: HoverEffect;
+
+    constructor() {
+        super();
+        this.hoverEffect = new HoverEffect(this.startSprite);
+    }
 
     update(c: Context) {
         if (c.keyJustPressed("KeyE")) {
